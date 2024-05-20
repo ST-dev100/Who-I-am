3da39e2d6268f3f793b578919a0d57bfa32ae302
sima <simalike245@gmail.com>
2024-05-20 08:36:14 -0700

    inserted

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import {BrowserRouter} from 'react-router-dom';
+import { Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas} from './components'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <BrowserRouter>
+        <div className='relative z-0 bg-primary'>
+          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+            <Navbar/>
+            <Hero/>
+          </div>
+          <About/>
+          <Experience/>
+          <Tech/>
+          <Works/>
+          <Feedbacks/>
+          <div className='relative z-0'>
+            <Contact/>
+            <StarsCanvas/>
+          </div>
+        </div>
+      </BrowserRouter>
+  )
+}
+
+export default App
