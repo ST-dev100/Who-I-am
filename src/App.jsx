e4efe5879f022804517effd8653b6f49896d9306
sima <simalike245@gmail.com>
2024-05-30 04:45:33 -0700

    o

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,15 @@ function App() {
           <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
             <Navbar/>
             <Hero/>
-          
+          </div>
+          <About/>
+          <Experience/>
+          <Tech/>
+          <Works/>
+          <Feedbacks/>
+          <div className='relative z-0'>
+            <Contact/>
+            <StarsCanvas/>
           </div>
         </div>
       </BrowserRouter>
