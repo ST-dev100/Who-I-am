e37a0a6580326f58ca94fdbff69d62d338f7ca2e
sima <simalike245@gmail.com>
2024-05-30 02:15:05 -0700

    3d

--- src/App.jsx
+++ src/App.jsx
@@ -22,15 +22,7 @@ function App() {
           <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
             <Navbar/>
             <Hero/>
-          </div>
-          <About/>
-          <Experience/>
-          <Tech/>
-          <Works/>
-          <Feedbacks/>
-          <div className='relative z-0'>
-            <Contact/>
-            <StarsCanvas/>
+          
           </div>
         </div>
       </BrowserRouter>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { ComputersCanvas } from "./canvas";
 import {EarthCanvas} from "./canvas";
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+    <section className={`relative w-full h-[1000px] mx-auto`}>
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
@@ -23,7 +23,6 @@ const Hero = () => {
           </p>
         </div>
       </div>
-
       <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
