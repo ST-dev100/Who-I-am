d95e767b267ce25121010984327cf6284f2b2e2e
sima <simalike245@gmail.com>
2024-05-31 00:24:44 -0700

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ function App() {
             <Hero/>
           </div>
           <About/>
-          {/* <Experience/> */}
+          <Experience/>
           {/* <Tech/> */}
           <Works/>
-          <Feedbacks/>
+          {/* <Feedbacks/> */}
           <div className='relative z-0'>
             <Contact/>
             <StarsCanvas/>
